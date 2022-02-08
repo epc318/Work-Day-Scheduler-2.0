4a4720be10129b28529afeca8beea9fb184adf06
Rick Johnson <carl4917@umn.edu>
2022-02-07 21:02:31 -0600

    fixing enter ToDo funtion, adding pull/load all ToDos

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -6,13 +6,27 @@ let toDo = function() {
     localStorage.setItem("toDo",JSON.stringify(toDo));
 };
 
-let enterTask = function(key) {
+let enterToDo = function(key) {
  let timeInt = $(".row").find(".crunchTime");
-    for(let i = 0; i < timeDiv.length; i++) {
-        let taskTime = timeDiv[i];
+    for(let i = 0; i < timeInt.length; i++) {
+        let taskTime = timeInt[i];
         if(taskTime.innerHTML === key) {
-          let taskDiv = $(taskTime).closest(".row").find(".description");
-          $(taskDiv).text(tasks[key]);
+          let timeInt = $(taskTime).closest(".row").find(".description");
+          $(timeInt).text(toDo[key]);
         }
     }
 };
+
+let pullToDos = function() {
+    toDO = JSON.parse(localStorage.getItem("toDo"));
+    if(!toDO) {
+        toDO = {
+        "8AM": [],"9AM": [], "10AM": [],"11AM": [], "12PM": [],"1PM": [],"2PM": [],"3PM": [],"4PM": [],"5PM": [],
+        }
+    };
+    for(key in ToDo) {
+        enterTask(key);
+    }
+};
+
+pullToDos()
