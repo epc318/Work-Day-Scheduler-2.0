1425575975804320bf2ac5db5ff84f72fe960c4e
Rick Johnson <carl4917@umn.edu>
2022-02-07 20:47:06 -0600

    creating toDO item and save to local storage

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,2 +1,7 @@
 // Show Current date
 $("#currentDate").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
+
+let toDo = {};
+let saveTask = function() {
+    localStorage.setItem("toDO", JSON.stringify(toDO));
+};
